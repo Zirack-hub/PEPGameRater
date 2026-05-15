38ff6b976f9a2b2f74887f9da1390c89ef1e0273
Zirack <s.merino.prado@gmail.com>
2026-05-15 13:10:18 +0200

    feat: add Calificacion y Comentario models con migrations

--- gamerater/juegos/migrations/0001_initial.py
+++ gamerater/juegos/migrations/0001_initial.py
@@ -39,5 +39,32 @@ class Migration(migrations.Migration):
                 'ordering': ['-fecha_creacion'],
             },
         ),
-        
+        migrations.CreateModel(
+            name='Calificacion',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('puntuacion', models.IntegerField(choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6'), (7, '7'), (8, '8'), (9, '9'), (10, '10')])),
+                ('resena', models.TextField(verbose_name='Reseña')),
+                ('fecha_creacion', models.DateTimeField(auto_now_add=True)),
+                ('videojuego', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='calificaciones', to='juegos.videojuego')),
+                ('usuario', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='calificaciones', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['-fecha_creacion'],
+                'unique_together': {('videojuego', 'usuario')},
+            },
+        ),
+        migrations.CreateModel(
+            name='Comentario',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('texto', models.TextField()),
+                ('fecha_creacion', models.DateTimeField(auto_now_add=True)),
+                ('videojuego', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comentarios', to='juegos.videojuego')),
+                ('autor', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comentarios', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['-fecha_creacion'],
+            },
+        ),
     ]
